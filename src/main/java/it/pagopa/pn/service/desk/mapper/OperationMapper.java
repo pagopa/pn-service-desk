3bc282efd2e62a858da01551c145e5fb036710bd
cristopher.moccia <cristopher.moccia@accenture.com>
2023-08-24 13:27:38 +0200

    added method for generate request id. Added relative unit test

--- src/main/java/it/pagopa/pn/service/desk/mapper/OperationMapper.java
+++ src/main/java/it/pagopa/pn/service/desk/mapper/OperationMapper.java
@@ -1,16 +1,12 @@
 package it.pagopa.pn.service.desk.mapper;
 
-import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.NotificationStatus;
 import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.OperationResponse;
 import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.SDNotificationSummary;
 import it.pagopa.pn.service.desk.middleware.entities.PnServiceDeskAttachments;
 import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.CreateOperationRequest;
 import it.pagopa.pn.service.desk.middleware.entities.PnServiceDeskOperations;
 import it.pagopa.pn.service.desk.model.OperationStatusEnum;
-import it.pagopa.pn.service.desk.service.OperationsService;
-import jdk.dynalink.Operation;
-import org.apache.commons.lang3.StringUtils;
-import reactor.core.publisher.Mono;
+import it.pagopa.pn.service.desk.utility.Utility;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
@@ -19,10 +15,12 @@ import java.util.List;
 
 public class OperationMapper {
 
+    private OperationMapper(){}
+
     public static PnServiceDeskOperations getInitialOperation (CreateOperationRequest operationRequest, String recipientInternalId){
 
         PnServiceDeskOperations pnServiceDeskOperations = new PnServiceDeskOperations();
-        pnServiceDeskOperations.setOperationId(getOperationId(operationRequest));
+        pnServiceDeskOperations.setOperationId(Utility.generateOperationId(operationRequest.getTicketId(), operationRequest.getTicketOperationId()));
         pnServiceDeskOperations.setTicketId(operationRequest.getTicketId());
         pnServiceDeskOperations.setStatus(OperationStatusEnum.CREATING.toString());
         pnServiceDeskOperations.setOperationStartDate(Instant.now());
@@ -33,12 +31,7 @@ public class OperationMapper {
     }
 
 
-    private static String getOperationId(CreateOperationRequest operationRequest){
-        String suffix = "000";
-        if (StringUtils.isBlank(operationRequest.getTicketOperationId()))
-            suffix = operationRequest.getTicketOperationId();
-        return operationRequest.getTicketId().concat(suffix);
-    }
+
 
     public static OperationResponse operationResponseMapper(PnServiceDeskOperations pnServiceDeskOperations){
 
---
+++ src/test/java/it/pagopa/pn/service/desk/utility/UtilityTest.java
@@ -0,0 +1,36 @@
+package it.pagopa.pn.service.desk.utility;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UtilityTest {
+
+
+    @Test
+    void testGenerateRequestId(){
+        String requestId = Utility.generateRequestId("12345");
+        Assertions.assertEquals("SERVICE_DESK_OPID-12345", requestId);
+    }
+
+    @Test
+    void testExtractOperationId(){
+        String operationId = Utility.extractOperationId("SERVICE_DESK_OPID-12345");
+        Assertions.assertEquals("12345", operationId);
+
+        operationId = Utility.extractOperationId("1234556");
+        Assertions.assertNull(operationId);
+    }
+
+    @Test
+    void testGenerateOperationId(){
+        String operationId = Utility.generateOperationId("1234", null);
+        Assertions.assertEquals("1234000", operationId);
+
+        operationId = Utility.generateOperationId("1234", "     ");
+        Assertions.assertEquals("1234000", operationId);
+
+        operationId = Utility.generateOperationId("1234","a-dfr");
+        Assertions.assertEquals("1234a-dfr", operationId);
+    }
+
+}
