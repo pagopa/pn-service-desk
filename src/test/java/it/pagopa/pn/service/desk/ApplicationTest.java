4af4b523ca177fa84aadfbe57a9fb6ff8ffc0d97
cristopher.moccia <cristopher.moccia@accenture.com>
2023-08-24 10:16:24 +0200

    added first sample test

---
+++ src/test/java/it/pagopa/pn/service/desk/ApplicationTest.java
@@ -0,0 +1,17 @@
+package it.pagopa.pn.service.desk;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+class ApplicationTest {
+
+    @Test
+    void firstTest(){
+        assertTrue(true);
+    }
+
+}
