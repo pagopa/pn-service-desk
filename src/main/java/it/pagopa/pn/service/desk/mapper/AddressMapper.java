50ac79717a8316d502f9060cdf419939761a6ef4
cristopher.moccia <cristopher.moccia@accenture.com>
2023-08-31 12:59:11 +0200

    added other properties

--- src/main/java/it/pagopa/pn/service/desk/mapper/AddressMapper.java
+++ src/main/java/it/pagopa/pn/service/desk/mapper/AddressMapper.java
@@ -1,11 +1,13 @@
 package it.pagopa.pn.service.desk.mapper;
 
 
+import it.pagopa.pn.service.desk.config.PnServiceDeskConfigs;
 import it.pagopa.pn.service.desk.generated.openapi.msclient.pnaddressmanager.v1.dto.AnalogAddressDto;
 import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.AnalogAddress;
 import it.pagopa.pn.service.desk.mapper.common.BaseMapper;
 import it.pagopa.pn.service.desk.mapper.common.BaseMapperImpl;
 import it.pagopa.pn.service.desk.middleware.entities.PnServiceDeskAddress;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -17,11 +19,13 @@ public class AddressMapper {
         throw new IllegalCallerException("the constructor must not called");
     }
 
-    public static PnServiceDeskAddress toEntity(AnalogAddress address, String operationId){
+    public static PnServiceDeskAddress toEntity(AnalogAddress address, String operationId, PnServiceDeskConfigs configs){
         PnServiceDeskAddress pnAddress = mapper.toEntity(address);
         pnAddress.setOperationId(operationId);
-        Instant instant = LocalDateTime.now().plusDays(120).toInstant(ZoneOffset.UTC);
-        pnAddress.setTtl(instant.getEpochSecond());
+        if (configs != null && configs.getTtlReceiverAddress() != null) {
+            Instant instant = LocalDateTime.now().plusDays(configs.getTtlReceiverAddress()).toInstant(ZoneOffset.UTC);
+            pnAddress.setTtl(instant.getEpochSecond());
+        }
 
         return pnAddress;
     }
