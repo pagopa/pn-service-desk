28be74072cffc878b4d360f506f7c57134d16603
cristopher.moccia <cristopher.moccia@accenture.com>
2023-09-01 13:41:07 +0200

    some fixes

--- src/main/java/it/pagopa/pn/service/desk/service/impl/OperationsServiceImpl.java
+++ src/main/java/it/pagopa/pn/service/desk/service/impl/OperationsServiceImpl.java
@@ -2,6 +2,7 @@ package it.pagopa.pn.service.desk.service.impl;
 
 import it.pagopa.pn.service.desk.config.PnServiceDeskConfigs;
 import it.pagopa.pn.service.desk.exception.PnGenericException;
+import it.pagopa.pn.service.desk.exception.PnRetryStorageException;
 import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.CreateOperationRequest;
 import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.OperationsResponse;
 import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.VideoUploadRequest;
@@ -21,6 +22,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
 import static it.pagopa.pn.service.desk.exception.ExceptionTypeEnum.OPERATION_ID_IS_PRESENT;
@@ -77,11 +79,29 @@ public class OperationsServiceImpl implements OperationsService {
     public Mono<VideoUploadResponse> presignedUrlVideoUpload(String xPagopaPnUid, String operationId, VideoUploadRequest videoUploadRequest) {
 
         return operationDAO.getByOperationId(operationId)
-                .flatMap(operation -> safeStorageClient.getPresignedUrl(videoUploadRequest))
+                .switchIfEmpty(Mono.error(new PnGenericException(OPERATION_ID_IS_PRESENT, OPERATION_ID_IS_PRESENT.getMessage(), HttpStatus.BAD_REQUEST)))
+                .flatMap(operation -> checkLoadedFile(operationId))
+                .switchIfEmpty(Mono.just(operationId))
+                .flatMap(operationID -> safeStorageClient.getPresignedUrl(videoUploadRequest))
                 .doOnNext(fileCreationResponse ->
                     operationsFileKeyDAO.updateVideoFileKey(OperationsFileKeyMapper.getOperationFileKey(fileCreationResponse.getKey(), operationId))
                 )
                 .map(OperationsFileKeyMapper::getVideoUpload);
+    }
+
+    private Mono<String> checkLoadedFile(String operationId){
+        //TODO change name method
+        return operationsFileKeyDAO.getOperationFileKey(operationId)
+                .flatMap(operationFileKey -> safeStorageClient.getFile(operationFileKey.getFileKey()))
+                .map(response -> operationId)
+                //TODO change exception message
+                .onErrorResume(PnRetryStorageException.class, ex -> Mono.error(new PnGenericException(OPERATION_ID_IS_PRESENT, OPERATION_ID_IS_PRESENT.getMessage(), HttpStatus.BAD_REQUEST)))
+                .onErrorResume(WebClientResponseException.class, ex -> {
+                    if (ex.getStatusCode() == HttpStatus.NOT_FOUND) return Mono.just(operationId);
+                    //TODO change exception message
+                    return Mono.error(new PnGenericException(OPERATION_ID_IS_PRESENT, OPERATION_ID_IS_PRESENT.getMessage(), HttpStatus.BAD_REQUEST));
+                });
+
     }
 
 
