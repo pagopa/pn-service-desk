9aaad1c9f986d1d2825e90264b2a68823dd13c23
pasqualino.cristaudo <>
2023-09-04 17:26:09 +0200

    Updated Paperchannel Prepare and Result quee's flow

--- src/main/java/it/pagopa/pn/service/desk/action/impl/PreparePaperChannelActionImpl.java
+++ src/main/java/it/pagopa/pn/service/desk/action/impl/PreparePaperChannelActionImpl.java
@@ -2,6 +2,7 @@ package it.pagopa.pn.service.desk.action.impl;
 
 import it.pagopa.pn.service.desk.action.PreparePaperChannelAction;
 import it.pagopa.pn.service.desk.config.PnServiceDeskConfigs;
+import it.pagopa.pn.service.desk.exception.PnGenericException;
 import it.pagopa.pn.service.desk.generated.openapi.msclient.pnpaperchannel.v1.dto.PrepareEventDto;
 import it.pagopa.pn.service.desk.generated.openapi.msclient.pnpaperchannel.v1.dto.StatusCodeEnumDto;
 import it.pagopa.pn.service.desk.mapper.PaperChannelMapper;
@@ -11,12 +12,16 @@ import it.pagopa.pn.service.desk.middleware.externalclient.pnclient.paperchannel
 import it.pagopa.pn.service.desk.model.OperationStatusEnum;
 import it.pagopa.pn.service.desk.utility.Utility;
 import lombok.AllArgsConstructor;
+import lombok.CustomLog;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import static it.pagopa.pn.service.desk.exception.ExceptionTypeEnum.ENTITY_NOT_FOUND;
+import static it.pagopa.pn.service.desk.exception.ExceptionTypeEnum.PAPERCHANNEL_STATUS_CODE_EMPTY;
 
 
 @Component
+@CustomLog
 @AllArgsConstructor
 public class PreparePaperChannelActionImpl implements PreparePaperChannelAction {
 
@@ -29,29 +34,42 @@ public class PreparePaperChannelActionImpl implements PreparePaperChannelAction 
 
     @Override
     public void execute(PrepareEventDto eventDto) {
-        //TODO - operation null -> switchIfEmpty
-        //TODO - status code from paperChannel empty -> if on statusCode
         String operationId = Utility.extractOperationId(eventDto.getRequestId());
         operationDAO.getByOperationId(operationId)
-                .flatMap(entityOperation -> {
-                    if(eventDto.getStatusCode() != null && StringUtils.isNotBlank(eventDto.getStatusCode().getValue())
-                            && StringUtils.equals(eventDto.getStatusCode().getValue(), StatusCodeEnumDto.KO.getValue())) {
-                        return updateOperationStatus(entityOperation, OperationStatusEnum.KO, eventDto.getStatusDetail());
-                    } else {
-                        return paperSendRequest(pnServiceDeskConfigs, entityOperation, eventDto);
-                    }
-                }).block();
-
+                .switchIfEmpty(Mono.error(() -> {
+                    log.debug("The operation entity was not found with this operationId: {}", operationId);
+                    return new PnGenericException(ENTITY_NOT_FOUND, ENTITY_NOT_FOUND.getMessage());
+                }))            .flatMap(entityOperation -> {
+                if(eventDto.getStatusCode() == null || StringUtils.isBlank(eventDto.getStatusCode().getValue())) {
+                    log.debug("The status code is null or empty");
+                    return Mono.error(new PnGenericException(PAPERCHANNEL_STATUS_CODE_EMPTY, PAPERCHANNEL_STATUS_CODE_EMPTY.getMessage()));
+                }
+                if(eventDto.getStatusCode() == StatusCodeEnumDto.PROGRESS) {
+                    return paperSendRequest(pnServiceDeskConfigs, entityOperation, eventDto);
+                }
+                OperationStatusEnum newStatus = Utility.getOperationStatusFrom(eventDto.getStatusCode());
+                return updateOperationStatus(entityOperation, newStatus, eventDto.getStatusDetail());
+            })
+            .onErrorResume(error -> {
+                OperationStatusEnum newStatus = Utility.getOperationStatusFrom(StatusCodeEnumDto.KO);
+                return operationDAO
+                        .getByOperationId(operationId)
+                        .flatMap(entityOperation ->
+                                updateOperationStatus(entityOperation, newStatus, error.getMessage()));
+            })
+            .block();
     }
 
     private Mono<Void> paperSendRequest(PnServiceDeskConfigs pnServiceDeskConfigs, PnServiceDeskOperations entityOperation, PrepareEventDto prepareEventDto){
         String requestId = Utility.generateRequestId(entityOperation.getOperationId());
+        log.debug("Executing Paperchannel Send with requestId: {}", requestId);
         return paperChannelClient.sendPaperSendRequest(requestId, PaperChannelMapper.getPaperSendRequest(pnServiceDeskConfigs, entityOperation, prepareEventDto))
                 .flatMap(response -> updateOperationStatus(entityOperation, OperationStatusEnum.PROGRESS, ""))
                 .onErrorResume(error -> updateOperationStatus(entityOperation, OperationStatusEnum.KO, error.getMessage()));
     }
 
     private Mono<Void> updateOperationStatus(PnServiceDeskOperations entityOperation, OperationStatusEnum operationStatusEnum, String errorReason){
+        log.debug("Update operation entity and event with new status: {}", operationStatusEnum.toString());
         entityOperation.setStatus(operationStatusEnum.toString());
         if(StringUtils.isNotBlank(errorReason)) {
             entityOperation.setErrorReason(errorReason);
--- src/main/java/it/pagopa/pn/service/desk/exception/ExceptionTypeEnum.java
+++ src/main/java/it/pagopa/pn/service/desk/exception/ExceptionTypeEnum.java
@@ -11,7 +11,9 @@ public enum ExceptionTypeEnum {
     DATA_VAULT_DECRYPTION_ERROR("DATA_VAULT_DECRYPTION_ERROR", "Servizio di anonimizzazione non disponibile."),
     DOCUMENT_URL_NOT_FOUND("DOCUMENT_URL_NOT_FOUND", "Url allegato non disponibile"),
     ADDRESS_IS_NOT_VALID("ADDRESS_IS_NOT_VALID", "l'indirizzo non è valido"),
-    OPERATION_ID_IS_PRESENT("OPERATION_ID_IS_PRESENT", "L'operation id è già presente per quel tiket id");
+    OPERATION_ID_IS_PRESENT("OPERATION_ID_IS_PRESENT", "L'operation id è già presente per quel tiket id"),
+    ENTITY_NOT_FOUND("ENTITY_NOT_FOUND", "L'entità ricercata non è presente nel Database"),
+    PAPERCHANNEL_STATUS_CODE_EMPTY("PAPERCHANNEL_STATUS_CODE_EMPTY", "Lo status code inviato dal microservizio Paperchannel è mancante");
 
     private final String title;
     private final String message;
