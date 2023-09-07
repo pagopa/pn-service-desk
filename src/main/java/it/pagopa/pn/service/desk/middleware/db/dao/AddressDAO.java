5ba13d73f77fbd3117425b4f68f94135c78ee15f
cristopher.moccia <cristopher.moccia@accenture.com>
2023-09-07 09:28:19 +0200

    added transaction for creation of operation and address

--- src/main/java/it/pagopa/pn/service/desk/middleware/db/dao/AddressDAO.java
+++ src/main/java/it/pagopa/pn/service/desk/middleware/db/dao/AddressDAO.java
@@ -1,12 +1,15 @@
 package it.pagopa.pn.service.desk.middleware.db.dao;
 
+
 import it.pagopa.pn.service.desk.middleware.entities.PnServiceDeskAddress;
 import reactor.core.publisher.Mono;
+import software.amazon.awssdk.enhanced.dynamodb.model.TransactWriteItemsEnhancedRequest;
 
 public interface AddressDAO {
 
-  Mono<PnServiceDeskAddress> createAddress (PnServiceDeskAddress address);
 
   Mono<PnServiceDeskAddress> getAddress (String operationId);
+
+  void createWithTransaction(TransactWriteItemsEnhancedRequest.Builder builder, PnServiceDeskAddress pnAddress);
 
 }
