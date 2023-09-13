87dc42e9f99c875b80ffd235eabf449f1395cf2a
cristopher.moccia <cristopher.moccia@accenture.com>
2023-09-13 10:36:24 +0200

    added properties ref into MockServerBean

--- src/test/java/it/pagopa/pn/service/desk/config/MockServerBean.java
+++ src/test/java/it/pagopa/pn/service/desk/config/MockServerBean.java
@@ -2,9 +2,11 @@ package it.pagopa.pn.service.desk.config;
 
 
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.mockserver.configuration.ConfigurationProperties;
 import org.mockserver.integration.ClientAndServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -13,6 +15,7 @@ import java.io.IOException;
 @Slf4j
 public class MockServerBean {
     private ClientAndServer mockServer;
+    @Value("${mockserver.bean.port}")
     private final int port;
 
     public MockServerBean(int port){
