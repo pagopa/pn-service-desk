b62d3b1e1883ef1c534bf374ac157d22eb8fb49c
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2023-09-15 17:57:27 +0200

    adds log

--- src/main/java/it/pagopa/pn/service/desk/mapper/OperationMapper.java
+++ src/main/java/it/pagopa/pn/service/desk/mapper/OperationMapper.java
@@ -9,6 +9,7 @@ import it.pagopa.pn.service.desk.middleware.entities.PnServiceDeskEvents;
 import it.pagopa.pn.service.desk.middleware.entities.PnServiceDeskOperations;
 import it.pagopa.pn.service.desk.model.OperationStatusEnum;
 import it.pagopa.pn.service.desk.utility.Utility;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+@Slf4j
 public class OperationMapper {
 
     private OperationMapper(){}
@@ -61,6 +63,7 @@ public class OperationMapper {
         status.setStatus(NotificationStatus.StatusEnum.fromValue(pnServiceDeskOperations.getStatus()));
         status.setStatusDescription(pnServiceDeskOperations.getErrorReason());
         if (pnServiceDeskOperations.getEvents() != null && !pnServiceDeskOperations.getEvents().isEmpty()) {
+            pnServiceDeskOperations.getEvents().stream().forEach(e -> log.info("statusCode {}", e.getTimestamp()!= null ? e.getTimestamp() : "is null"));
             PnServiceDeskEvents e = pnServiceDeskOperations.getEvents().stream()
                     .filter(events -> events.getTimestamp() != null)
                     .max(Comparator.comparing(PnServiceDeskEvents::getTimestamp))
