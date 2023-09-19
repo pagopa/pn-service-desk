dd23f7b6cbce232bacde662563a98b5dae057828
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2023-09-19 18:40:19 +0200

    checkIunsToSend for unreachable api

---
+++ src/main/java/it/pagopa/pn/service/desk/service/impl/BaseService.java
@@ -0,0 +1,71 @@
+package it.pagopa.pn.service.desk.service.impl;
+
+import it.pagopa.pn.service.desk.middleware.db.dao.OperationDAO;
+import it.pagopa.pn.service.desk.middleware.entities.PnServiceDeskOperations;
+import it.pagopa.pn.service.desk.model.OperationStatusEnum;
+import lombok.CustomLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@CustomLog
+public class BaseService {
+
+    @Autowired
+    private OperationDAO operationDAO;
+
+    protected Mono<Long> checkNotificationFailed(String taxId, List<String> iuns) {
+        return this.operationDAO.searchOperationsFromRecipientInternalId(taxId)
+                .collectList()
+                .flatMap(operation -> {
+                    if (operation.isEmpty()) return Mono.just(1L);
+                    else return operationContainsIuns(operation, iuns);
+                });
+    }
+
+    protected Mono<Long> operationContainsIuns(List<PnServiceDeskOperations> operations, List<String> iuns){
+        Map<String, String> iunAndStatus = new HashMap<>();
+
+        operations.parallelStream().forEach(operation -> {
+            log.info("operationId: {} ", operation.getOperationId());
+
+            if (operation.getAttachments() != null && !operation.getAttachments().isEmpty()) {
+                List<String> pnIuns = operation.getAttachments().stream().map(a -> a.getIun()).collect(Collectors.toList());
+                List<String> commonIuns = iuns.stream().filter(i -> pnIuns.contains(i)).collect(Collectors.toList());
+
+                if (!commonIuns.isEmpty()) {
+                    commonIuns.stream().forEach(i -> {
+                        // set iun and its status. Status different from KO has priority
+                        if (iunAndStatus.containsKey(i)) {
+                            if (iunAndStatus.get(i).equals(OperationStatusEnum.KO.toString())) {
+                                iunAndStatus.put(i, operation.getStatus());
+                            }
+                        } else {
+                            iunAndStatus.put(i, operation.getStatus());
+                        }
+                    });
+                } else {
+                    iuns.stream().forEach(i -> iunAndStatus.put(i, OperationStatusEnum.KO.toString()));
+                }
+            }
+        });
+
+        return checkIunsToSend(iunAndStatus);
+    }
+
+    private Mono<Long> checkIunsToSend(Map<String, String> iunAndStatus) {
+        List<String> iunToSend =  iunAndStatus.entrySet().stream()
+                .filter(w -> w.getValue().equals(OperationStatusEnum.KO.toString()))
+                .map(s -> {
+                    log.info("iun to send {} ", s.getKey());
+                    return s.getKey();
+                })
+                .collect(Collectors.toList());
+        return iunToSend.isEmpty() ? Mono.just(0L) : Mono.just(1L);
+    }
+
+}
