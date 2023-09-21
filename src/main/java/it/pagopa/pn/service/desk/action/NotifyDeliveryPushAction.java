faba285f1f0f778286d8c3b5c5d7bdffeb5c44d3
DIR\luca.ciccarelli <luca.ciccarelli@accenture.com>
2023-09-21 18:41:01 +0200

    send event delivery push notifyNotificationViewer (NotifyDeliveryPushAction)

---
+++ src/main/java/it/pagopa/pn/service/desk/action/NotifyDeliveryPushAction.java
@@ -0,0 +1,7 @@
+package it.pagopa.pn.service.desk.action;
+
+import it.pagopa.pn.service.desk.action.common.BaseAction;
+import it.pagopa.pn.service.desk.model.NotifyEventDTO;
+
+public interface NotifyDeliveryPushAction extends BaseAction<NotifyEventDTO> {
+}
--- src/main/java/it/pagopa/pn/service/desk/middleware/responsehandler/InternalEventResponseHandler.java
+++ src/main/java/it/pagopa/pn/service/desk/middleware/responsehandler/InternalEventResponseHandler.java
@@ -2,8 +2,10 @@ package it.pagopa.pn.service.desk.middleware.responsehandler;
 
 
 
+import it.pagopa.pn.service.desk.action.NotifyDeliveryPushAction;
 import it.pagopa.pn.service.desk.action.ValidationOperationAction;
 import it.pagopa.pn.service.desk.middleware.queue.model.InternalEventBody;
+import it.pagopa.pn.service.desk.model.NotifyEventDTO;
 import lombok.AllArgsConstructor;
 import lombok.CustomLog;
 import org.springframework.stereotype.Component;
@@ -14,11 +16,23 @@ import org.springframework.stereotype.Component;
 public class InternalEventResponseHandler {
     private ValidationOperationAction validationOperationAction;
 
+    private NotifyDeliveryPushAction notifyDeliveryPushAction;
+
 
     public void handleInternalEventResponse(InternalEventBody response) {
         try {
             log.logStartingProcess("VALIDATION OPERATION");
             this.validationOperationAction.execute(response.getOperationId());
+            log.logEndingProcess("VALIDATION OPERATION");
+        } catch (Exception ex) {
+            log.logEndingProcess("ENDING WITH EXCEPTION");
+        }
+    }
+
+    public void handleNotifyDeliveryPushEventResponse(NotifyEventDTO response) {
+        try {
+            log.logStartingProcess("VALIDATION OPERATION");
+            this.notifyDeliveryPushAction.execute(response);
             log.logEndingProcess("VALIDATION OPERATION");
         } catch (Exception ex) {
             log.logEndingProcess("ENDING WITH EXCEPTION");
