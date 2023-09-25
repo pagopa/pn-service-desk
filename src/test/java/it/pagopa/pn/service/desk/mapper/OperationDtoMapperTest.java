6f4aea79568e29b2f84b2a74f5c9e0ddf3bcd96e
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2023-09-25 15:34:46 +0200

    adds test

---
+++ src/test/java/it/pagopa/pn/service/desk/mapper/OperationDtoMapperTest.java
@@ -0,0 +1,24 @@
+package it.pagopa.pn.service.desk.mapper;
+
+import it.pagopa.pn.service.desk.middleware.entities.PnServiceDeskOperations;
+import it.pagopa.pn.service.desk.model.OperationDto;
+import it.pagopa.pn.service.desk.model.OperationStatusEnum;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class OperationDtoMapperTest {
+
+    @Test
+    void initOperation() {
+        PnServiceDeskOperations p = new PnServiceDeskOperations();
+        p.setStatus(OperationStatusEnum.KO.toString());
+        p.setOperationId("1");
+        OperationDto dto = OperationDtoMapper.initOperation(p, "123");
+        assertNotNull(dto);
+        assertEquals(dto.getOperationId(), p.getOperationId());
+        assertEquals(dto.getIun(), "123");
+    }
+
+}
