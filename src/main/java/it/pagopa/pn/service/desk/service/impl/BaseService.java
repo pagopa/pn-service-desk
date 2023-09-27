c0f29627213c1af931791826c2dcee584d5fdeac
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2023-09-27 11:31:40 +0200

    remove unused import

--- src/main/java/it/pagopa/pn/service/desk/service/impl/BaseService.java
+++ src/main/java/it/pagopa/pn/service/desk/service/impl/BaseService.java
@@ -12,7 +12,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
