0c929cdda297d601913194dfb0b1b13294eed06b
DIR\luca.ciccarelli <luca.ciccarelli@accenture.com>
2023-11-23 15:00:23 +0100

    added auditLog in ApiKeysServiceImpl

--- src/main/java/it/pagopa/pn/service/desk/service/impl/ApiKeysServiceImpl.java
+++ src/main/java/it/pagopa/pn/service/desk/service/impl/ApiKeysServiceImpl.java
@@ -1,5 +1,8 @@
 package it.pagopa.pn.service.desk.service.impl;
 
+import it.pagopa.pn.commons.log.PnAuditLogBuilder;
+import it.pagopa.pn.commons.log.PnAuditLogEvent;
+import it.pagopa.pn.commons.log.PnAuditLogEventType;
 import it.pagopa.pn.service.desk.exception.PnGenericException;
 import it.pagopa.pn.service.desk.generated.openapi.server.v1.dto.ResponseApiKeys;
 import it.pagopa.pn.service.desk.mapper.ApiKeysMapper;
@@ -17,16 +20,23 @@ public class ApiKeysServiceImpl implements ApiKeysService {
 
     private final ApiKeysManagerClient apiKeysManagerClient;
 
+    private final PnAuditLogBuilder auditLogBuilder = new PnAuditLogBuilder();
+
     public ApiKeysServiceImpl(ApiKeysManagerClient apiKeysManagerClient) {
         this.apiKeysManagerClient = apiKeysManagerClient;
     }
 
     @Override
     public Mono<ResponseApiKeys> getApiKeys(String paId) {
+        PnAuditLogEvent logEvent = auditLogBuilder.before(PnAuditLogEventType.AUD_NT_INSERT, "getApiKeys for paId={}", paId)
+                .build().log();
         return apiKeysManagerClient.getBoApiKeys(paId).switchIfEmpty(Mono.empty()).onErrorResume(exception -> {
             log.error("errorReason = {}, An error occurred while calling the service to obtain api keys", exception.getMessage());
+            logEvent.generateFailure("errorReason = {}, An error occurred while calling the service to obtain api keys" + exception.getMessage()).log();
             return Mono.error(new PnGenericException(ERROR_ON_KEYS_MANAGER_CLIENT, exception.getMessage()));
-        }).flatMap(responseApiKeysDto ->
-                Mono.just(ApiKeysMapper.responseApiKeys(responseApiKeysDto)));
+        }).flatMap(responseApiKeysDto ->{
+            logEvent.generateSuccess("getApiKeys responseApiKeys: items={}, totalElement={}", responseApiKeysDto.getItems(), responseApiKeysDto.getTotal()).log();
+            return Mono.just(ApiKeysMapper.responseApiKeys(responseApiKeysDto));
+                });
     }
 }
