97c62622d04da1394a9668b75a0827e94ed02f42
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-12-12 12:54:51 +0100

    aggiunti FIXME sui test

--- src/test/java/it/pagopa/pn/service/desk/rest/NotificationAndMessageControllerTest.java
+++ src/test/java/it/pagopa/pn/service/desk/rest/NotificationAndMessageControllerTest.java
@@ -29,6 +29,7 @@ class NotificationAndMessageControllerTest {
                 .thenReturn(Mono.just(new PnClientID()));
     }
 
+    //FIXME aggiungere qualche test anche di KO, ad esempio se pnDeliveryClient.searchNotificationsPrivate restituisce 400, quale status http restituiamo al client?
     @Test
     void searchCourtesyMessagesFromTaxIdTest(){
         SearchNotificationsResponse response = new SearchNotificationsResponse();
