2596d44b398fc75f7c926a268f76769897a36fa0
mdisco-dgs <marco.disco@dgsspa.com>
2025-08-21 12:16:33 +0200

    PN-15867: add templateEngine in ExternalChannelApiConfigurator

--- src/main/java/it/pagopa/pn/service/desk/config/msclient/ExternalChannelApiConfigurator.java
+++ src/main/java/it/pagopa/pn/service/desk/config/msclient/ExternalChannelApiConfigurator.java
@@ -4,11 +4,21 @@ import it.pagopa.pn.commons.pnclients.CommonBaseClient;
 import it.pagopa.pn.service.desk.config.PnServiceDeskConfigs;
 import it.pagopa.pn.service.desk.generated.openapi.msclient.pnexternalchannel.v1.ApiClient;
 import it.pagopa.pn.service.desk.generated.openapi.msclient.pnexternalchannel.v1.api.DigitalCourtesyMessagesApi;
+import it.pagopa.pn.service.desk.mapper.ExternalChannelMapper;
+import it.pagopa.pn.service.desk.middleware.externalclient.pnclient.templatesengine.PnTemplatesEngineClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 public class ExternalChannelApiConfigurator extends CommonBaseClient {
+
+    private final PnTemplatesEngineClient pnTemplatesEngineClient;
+
+    public ExternalChannelApiConfigurator(PnTemplatesEngineClient pnTemplatesEngineClient) {
+        this.pnTemplatesEngineClient = pnTemplatesEngineClient;
+    }
 
     @Bean
     public DigitalCourtesyMessagesApi getDigitalCoutesyMessageApiExternalChannel (PnServiceDeskConfigs pnServiceDeskConfigs){
@@ -16,9 +26,14 @@ public class ExternalChannelApiConfigurator extends CommonBaseClient {
         ApiClient apiClient =
                 new ApiClient(super.initWebClient(ApiClient.buildWebClientBuilder()));
 
-apiClient.setBasePath(pnServiceDeskConfigs.getExternalChannelBasePath());
+        apiClient.setBasePath(pnServiceDeskConfigs.getExternalChannelBasePath());
 
         return new DigitalCourtesyMessagesApi(apiClient);
+    }
+
+    @PostConstruct
+    public void init() {
+        ExternalChannelMapper.setPnTemplatesEngineClient(pnTemplatesEngineClient);
     }
 
 }
