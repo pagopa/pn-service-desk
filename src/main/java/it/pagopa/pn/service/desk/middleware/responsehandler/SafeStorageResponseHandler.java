191a4c682e1863b4249a52b23fc02c9f7a528ce5
riccardo.pompili <riccardo.pompili@dgsspa.com>
2026-03-05 11:37:52 +0100

    PN-18410 - SafeStorageResponseHandler Adjustment

--- src/main/java/it/pagopa/pn/service/desk/middleware/responsehandler/SafeStorageResponseHandler.java
+++ src/main/java/it/pagopa/pn/service/desk/middleware/responsehandler/SafeStorageResponseHandler.java
@@ -2,6 +2,7 @@ package it.pagopa.pn.service.desk.middleware.responsehandler;
 
 import it.pagopa.pn.api.dto.events.GenericEventHeader;
 import it.pagopa.pn.service.desk.generated.openapi.msclient.safestorage.model.FileDownloadResponse;
+import it.pagopa.pn.service.desk.middleware.db.dao.OperationDAO;
 import it.pagopa.pn.service.desk.middleware.db.dao.OperationsFileKeyDAO;
 import it.pagopa.pn.service.desk.middleware.queue.model.InternalEvent;
 import it.pagopa.pn.service.desk.middleware.queue.model.InternalEventBody;
@@ -11,10 +12,13 @@ import it.pagopa.pn.service.desk.utility.Const;
 import lombok.AllArgsConstructor;
 import lombok.CustomLog;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-
 
 @Component
 @CustomLog
@@ -23,23 +27,32 @@ public class SafeStorageResponseHandler {
 
     private InternalQueueMomProducer internalQueueMomProducer;
     private OperationsFileKeyDAO operationsFileKeyDAO;
+    private OperationDAO operationDAO;
 
     public void handleSafeStorageResponse(FileDownloadResponse response) {
         operationsFileKeyDAO.getOperationFileKey(response.getKey())
-                .doOnNext(operationFileKey ->
-                        internalQueueMomProducer.push(getInternalEvent(operationFileKey.getOperationId()))
-                ).block();
+                            .flatMapMany(ofk ->
+                                     operationDAO.getByOperationId(ofk.getOperationId())
+                                         .flatMapMany(op ->
+                                              op.getIun() != null && !op.getIsSubOperation()
+                                                      ? Flux.just(ofk.getOperationId())
+                                                      : Flux.fromIterable( Optional.ofNullable(op.getSubOperationsIds()).orElse(List.of()) )
+                                                     )
+                                        )
+                            .flatMap(id ->
+                                             Mono.fromRunnable(() -> internalQueueMomProducer.push(getInternalEvent(id)) )
+                                    )
+                            .then()
+                            .block();
     }
 
-
     private InternalEvent getInternalEvent(String operationId){
-
         GenericEventHeader prepareHeader= GenericEventHeader.builder()
-                .publisher(Const.PUBLISHER_PREPARE)
-                .eventId(UUID.randomUUID().toString())
-                .createdAt(Instant.now())
-                .eventType(EventTypeEnum.VALIDATION_OPERATIONS_EVENTS.name())
-                .build();
+            .publisher(Const.PUBLISHER_PREPARE)
+            .eventId(UUID.randomUUID().toString())
+            .createdAt(Instant.now())
+            .eventType(EventTypeEnum.VALIDATION_OPERATIONS_EVENTS.name())
+            .build();
 
         InternalEventBody internalEventBody = new InternalEventBody();
         internalEventBody.setOperationId(operationId);
